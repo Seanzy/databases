480e52213882253f3e7b1e29d7bed6f1e254d23a
Hack Reactor Student <student@hackreactor.com>
2017-11-04 16:15:40 -0700

    implemented post

--- server/models/index.js
+++ server/models/index.js
@@ -35,33 +35,60 @@ module.exports = {
         res.send(messageArr);
         
       });
-
-      
-      
     }, // a function which produces all the messages
     post: function (req) {
+      console.log("in post");
       var body = '';
       req.on('data', function(chunks) {
         body += chunks;
       });
       req.on('end', function(err, data) {
         body = qs.parse(body);
-        var username = body.username;
+        var userName = body.username;
         var message = body.text;
         var roomName = body.roomname;
         
-        db.query((`INSERT INTO messages (userId, messages, roomId) VALUES (${req} , ${req}, ${req})`), function(err, rows, fields){});
-        
-        
-      });
-      
-      
-      
-      
-      
-    } // a function which can be used to insert a message into the database
-  },
+        console.log("body is ", body);
+        //grabs user ID
+        db.query((`SELECT userId FROM users WHERE users.username = ${userName}`), function(err, rows, fields) {
+          if (err) {
+            throw err; 
+          } 
+          var userId = rows[0].userId;
+          console.log("user id" , userId);
+          db.query((`SELECT roomId FROM rooms WHERE rooms.roomname = ${roomName}`), function(err, rows, fields) {
+            if (err) {
+              throw err; 
+            } 
+            var roomId = rows[0].roomId;
+            db.query((`INSERT INTO messages (userId, messages, roomId) VALUES (${userId}, ${message}, ${roomId})`), 
+              function(err, rows, fields) { 
+                if (err) {
+                  throw err; 
+                } 
+                return 0; 
+              
+              });      
+          });
+          
+          
+          
+          
+          // var userIdQuery = `SELECT userId FROM users WHERE users.username = ${username}`;
+          // var roomIdQuery = `SELECT roomId FROM rooms WHERE rooms.roomname = ${roomName}`;  
+          
+          // console.log('*******************************', userIdQuery);
+          // console.log(roomIdQuery);
+          
+          // db.query((`INSERT INTO messages (userId, messages, roomId) VALUES (${req} , ${message}, ${req})`));
+          // db.query((`INSERT INTO messages (userId, messages, roomId) VALUES (7, 'yo momma', 5)`));
+          // db.query(`INSERT INTO messages (userId, messages, roomId) VALUES ((SELECT userId FROM users WHERE users.username = ${username}) , ${message}, (SELECT roomId FROM rooms WHERE rooms.roomname = ${roomName}))`);
 
+        });
+      });
+    } 
+  },// a function which can be used to insert a message into the database
+  
   users: {
     // Ditto as above.
     get: function (req, res) { 
@@ -75,18 +102,22 @@ module.exports = {
         
         res.send(userObj);
       });
-      
-      
     },
     post: function (req) {
-      db.query((`INSERT INTO users (userId, username) VALUES (${req} , ${req})`), function(err, rows, fields){});
-      
-  
+      var body = '';
+      req.on('data', function(chunks){
+        body += chunks;
+      });
+      req.on('end', function() {
+        body = qs.parse(body);
+        var username = body.username;
+        db.query((`INSERT INTO users (userId, username) VALUES (${req} , ${username})`));
+        
+        
+      });
     } 
   }
 };
-
-
 
 
 
